32d40908e94d5ad543e0547584a7cac8b0380ac5
randmon <ccmarques98@gmail.com>
2022-06-21 00:44:10 +0200

    gravatar for users

--- apps/airlines_ipmajor_web/lib/airlines_ipmajor_web/views/layout_view.ex
+++ apps/airlines_ipmajor_web/lib/airlines_ipmajor_web/views/layout_view.ex
@@ -8,4 +8,15 @@ defmodule AirlinesIpmajorWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def gravatar(email) do
+    hash = email
+    |> String.trim()
+    |> String.downcase()
+    |> :erlang.md5()
+    |> Base.encode16(case: :lower)
+
+    img = "http://www.gravatar.com/avatar/#{hash}?s=30&d=identicon"
+    img_tag(img)
+  end
 end
--- apps/airlines_ipmajor_web/lib/airlines_ipmajor_web/views/user_view.ex
+++ apps/airlines_ipmajor_web/lib/airlines_ipmajor_web/views/user_view.ex
@@ -1,3 +1,14 @@
 defmodule AirlinesIpmajorWeb.UserView do
   use AirlinesIpmajorWeb, :view
+
+  def gravatar(email) do
+    hash = email
+    |> String.trim()
+    |> String.downcase()
+    |> :erlang.md5()
+    |> Base.encode16(case: :lower)
+
+    img = "http://www.gravatar.com/avatar/#{hash}?s=150&d=identicon"
+    img_tag(img)
+  end
 end
